8295495d806df11f5d9c0e0756172a43ec47fa68
Maxime Bézot <60543092+MaxxBzt@users.noreply.github.com>
2023-09-23 22:58:32 -0700

    Add Dynamic endpoint and better UI

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-console.log("Hello World!");
-console.log("This website will allow you to rate your favorite TV show.");
-console.log("And write a review about it.");
+function sayHello() {
+    console.log("Hello World!");
+    console.log("This website will allow you to rate your favorite TV show.");
+    console.log("And write a review about it.");
+}
+
+
+module.exports = { sayHello };
--- start.js
+++ start.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const { sayHello } = require('.');
 
-app.use(express.static('views'));
+// Serve static files from the 'views' directory
+app.use(express.static(path.join(__dirname, 'views')));
 
+// For the home page, send the index.html 
 app.get('/', (req, res) => {
-    console.log("Hello World!");
-    res.render('index');
-})
+  console.log("This is the blank page!");
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
-app.listen(5500);
+// For the shows page, send the shows.html
+app.get('/shows', (req, res) => {
+    console.log("This is the shows page!");
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  });
+
+// Dynamic endpoint that accepts an argument
+  app.get('/show', (req, res) => {
+    const argument = req.query.argument; // Retrieve the argument from the user
+
+    sayHello(); // Call the function from the imported module (assignment 1)
+  
+    // Check if the argument is provided
+    if (!argument) {
+      return res.status(400).json({ error: 'Please provide an argument.' });
+    }
+  
+    const responseData = {
+      message: `You provided the argument: ${argument}`,
+    };
+  
+    res.json(responseData);
+  });
+  
+
+const port = process.env.PORT || 5500;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
